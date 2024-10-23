551b3610d658cc81683ddda82eb21cb44bd922c4
Colin Jacobs <colin@coljac.net>
2024-10-23 11:33:01 +1100

    header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,21 +13,20 @@ const Header: React.FC<HeaderProps> = ({ openSettings }) => {
     <header className="bg-green-600 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/newest" className="text-2xl font-bold flex items-center">
-          {/* <span className="font-fairfax-pona-hd mr-2">akesi</span> */}
           <img src={logo} alt="Akesi Logo" className="h-8 w-8 mr-2" />
           Akesi
         </Link>
         <nav>
-          <ul className="flex space-x-4 items-center">
-            <li><Link to="/newest" className="hover:underline font-fairfax-pona-hd">tomo</Link></li>
-            <li><Link to="/about" className="hover:underline font-fairfax-pona-hd">seme</Link></li>
+          <ul className="flex space-x-4 items-center text-2xl">
+            <li><Link to="/newest" className="hover:underline font-linja-pona">tomo</Link></li>
+            <li><Link to="/about" className="hover:underline font-linja-pona">seme</Link></li>
             <li>
               <button
                 onClick={openSettings}
                 className="hover:underline flex items-center"
               >
-                <Settings size={18} className="mr-1" />
-                sitelen/<span className="font-fairfax-pona-hd">sitelen</span>
+                <Settings size={24} className="mr-1" />
+                sitelen/<span className="font-linja-pona">sitelen</span>
               </button>
             </li>
           </ul>
